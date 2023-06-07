aaa151c39c4ee6549dfb5ac4ca0538a830d9e3b8
Juan Iñiguez <juann.sebastiann@gmail.com>
2023-06-07 18:52:28 -0300

    Casi-Terminado2

--- MiProyecto/index.js
+++ MiProyecto/index.js
@@ -12,8 +12,10 @@ if(localStorage.getItem('lsProductos')) {
   productos = JSON.parse(localStorage.getItem('lsProductos'))
 }
 
-let prodFiltrados = []
- 
+let prodFiltrados = productos
+
+//        DOM
+
 const fragment = document.createDocumentFragment();
 
 const cardproductos = document.querySelector('.dinamicproduct');
@@ -29,6 +31,10 @@ const imgpath = cardtemplate.querySelector('.img-product').getAttribute('src')
 
 const eOrdernar = document.querySelector('.ordenes')
 
+
+
+
+
 //         CARGA DE PAGINA
 
 document.addEventListener('DOMContentLoaded', e => { fetchData() });
@@ -36,11 +42,12 @@ document.addEventListener('DOMContentLoaded', e => { fetchData() });
 const fetchData = async () => {
 
   try {
-    const res = await fetch("../arts.JSON")
+    const res = await fetch("arts.JSON")
     const productos = await res.json()
     const lsProductos = localStorage.setItem("lsProductos", JSON.stringify(productos))
     pintarCards(productos)
     pintarFiltros(productos)
+    pintarMenu(productos)
     if (localStorage.getItem('lsCarrito')) {
         const carrito = JSON.parse(localStorage.getItem('lsCarrito'))
         pintarcarrito(carrito)
@@ -62,7 +69,7 @@ const fetchData = async () => {
       //const fragment = document.createDocumentFragment();
       cardproductos.innerHTML=''
       productos.forEach( (e) => { 
-      cardtemplate.querySelector(".card-body .card-text").textContent = e.descripcion;
+      /* cardtemplate.querySelector(".card-body .card-text").textContent = e.descripcion; */
       cardtemplate.querySelector(".img-product").src= `${imgpath}${e.sku}.webp`
       cardtemplate.querySelector('.card-footer .card-text').textContent = `${e.nombre}`
       cardtemplate.querySelector('.card-footer h6').textContent = `$ ${e.precio}   -   3 y 6 Cuotas sin interes.`
@@ -91,17 +98,18 @@ const pintarcarrito = obj =>{
     templatecarrito.querySelector('.carritoArticulo').textContent = `(${producto.codigo}) ${producto.nombre}`
     templatecarrito.querySelector('.carritoCantidad').textContent = producto.cantidad
     templatecarrito.querySelector('.carritoCantidad').setAttribute('data-value', producto.cantidad)
-    templatecarrito.querySelector('.carritoPrecio').textContent = "$ " +producto.precio
+    templatecarrito.querySelector('.carritoPrecio').textContent = `$  ${(producto.precio*1).toLocaleString('es-ES')}`
+    // el * 1 es porque sino no tomaba el toLocaleString y no se porque
     templatecarrito.querySelector('.carritoPrecio').setAttribute('data-value', producto.precio)
     let ntotal = producto.cantidad*producto.precio
-    templatecarrito.querySelector('.carritoTotalArt').textContent = `$ ${(producto.cantidad*producto.precio).toLocaleString('es-ES')}`
+    templatecarrito.querySelector('.carritoTotalArt').textContent = `$  ${(producto.cantidad*producto.precio).toLocaleString('es-ES')}`
     const clone = templatecarrito.cloneNode(true);
     fragment.appendChild(clone);
     templatecarrito.querySelector('.sumar').setAttribute('data-value', producto.codigo)
   })
   const nCantidad = Object.values(obj).reduce((acc, {cantidad}) => acc + cantidad, 0)
   const nTotal   = Object.values(obj).reduce((acc, {cantidad, precio}) => acc + cantidad * precio, 0)
-  divCarrito.innerHTML = `<h5 class="h4 text-white border text-end py-0 px-2 m-0 ">Cantidad total: ${nCantidad} <br>Total de la compra: $${nTotal}</h5>`
+  divCarrito.innerHTML = `<h5 class="h4 text-white border text-end py-0 px-2 m-0 ">Cantidad total: ${nCantidad} <br>Total de la compra: $${nTotal.toLocaleString('es-ES')}</h5>`
   divCarrito.prepend(fragment)
   const lsCarrito = localStorage.setItem("lsCarrito", JSON.stringify(obj))
   
@@ -197,10 +205,9 @@ const pintarcarrito = obj =>{
   //			Lista las categorias de los productos
 	const listarCategorias = productos =>{
 		productos.forEach( (e) => {
-			if (categorias.includes(e.categoria) ){
-		  	}else{
-      			categorias.push(e.categoria)
-		   }
+			if (!categorias.includes(e.categoria) ){
+        categorias.push(e.categoria)
+		  }
 		})
 	} 
 
@@ -221,7 +228,7 @@ const pintarcarrito = obj =>{
 	
   } 
 
-  //      Order By
+  //                  Order By
 
  const ordenarAsc = arr => { 
   arr.sort( (a,b) => {
@@ -237,8 +244,8 @@ const pintarcarrito = obj =>{
 })
 } 
 
-function ordenarDesc() { 
-  productos.sort( (a,b) => {
+const ordenarDesc = arr => { 
+  arr.sort( (a,b) => {
   const nameA = a.nombre.toUpperCase()
   const nameB = b.nombre.toUpperCase()
   if (nameA > nameB) {
@@ -251,12 +258,12 @@ function ordenarDesc() {
 }) 
 } 
 
-const menoramayor = () => {productos.sort((a,b) => a.precio - b.precio)}
-const mayoramenor = () => {productos.sort((a,b) => b.precio - a.precio)}
+const menoramayor = () => {prodFiltrados.sort((a,b) => a.precio - b.precio)}
+const mayoramenor = () => {prodFiltrados.sort((a,b) => b.precio - a.precio)}
 
-// Filtrar Productos
+//                      Filtrar Productos
 
-const filtrarArticulos = ((lista, checked) => {
+const filtrarArticulos = ( () => {
 
   const categoriasexistentes = document.querySelectorAll('.checkbox')
   
@@ -276,7 +283,6 @@ const filtrarArticulos = ((lista, checked) => {
       }else{
 
           contador++
-          // CORREGIR
           prodFiltrados = prodFiltrados.filter(function(e) {
             if(e.categoria != tempCategoria){
             return (e)
@@ -289,6 +295,18 @@ const filtrarArticulos = ((lista, checked) => {
   })
 })
 
+//               Buscar productos
+
+const buscarArticulo = ((buscado) => {
+  
+  prodFiltrados = productos.filter(function(e) {
+    if(e.nombre.toUpperCase().includes(buscado.value.toUpperCase())){
+      return (e)
+    }
+  })
+    
+  })
+
 
 
 //
@@ -300,26 +318,27 @@ const filtrarArticulos = ((lista, checked) => {
     addCarrito(e)
   })
 
-//                Escucha los botones sumar y restar
   divCarrito.addEventListener('click',(e) => {
     if(e.target.classList.contains('sumar'))
     {
      
-    sumarCarrito(e.currentTarget.parentElement.parentElement.parentElement)
+      sumarCarrito(e.currentTarget.parentElement.parentElement.parentElement)
 	e.stopPropagation
       
-  }else if(e.target.classList.contains('restar')){
-    restarCarrito(e.currentTarget.parentElement.parentElement.parentElement)
+}else if(e.target.classList.contains('restar')){
+  restarCarrito(e.currentTarget.parentElement.parentElement.parentElement)
 	e.stopPropagation
-  }
-  e.stopPropagation
-  })
+}
+e.stopPropagation
+})
 
-  //    Escucha los botones de Filtros
+//                Escucha los botones sumar y restar
+
   eOrdernar.addEventListener('click',(e) =>{
     if(e.target.classList.contains('ascendente')){
       ordenarAsc(prodFiltrados)
       pintarCards(prodFiltrados)
+      
     }else if(e.target.classList.contains('descendete')){
       ordenarDesc(prodFiltrados)
       pintarCards(prodFiltrados)
@@ -332,28 +351,95 @@ const filtrarArticulos = ((lista, checked) => {
     }
     e.stopPropagation
   })
+  
+  //    Escucha los botones de Filtros
 
   navfilters.addEventListener('change',(e) => {
 
 
     if (e.target.checked){
-      filtrarArticulos(e.currentTarget,  e.target.checked)
-      /* filtrarArticulos(e.target.getAttribute('value'), e.target.checked) */
+      filtrarArticulos()
       pintarCards(prodFiltrados)
+      e.stopPropagation
     }else{
-          filtrarArticulos(e.currentTarget,  e.target.checked)
-          /* filtrarArticulos(e.target.getAttribute('value'), e.target.checked) */
+          filtrarArticulos()
+          e.stopPropagation
+      
    pintarCards(prodFiltrados)
-      /* pintarCards(articulo) */
+      
     }
     e.stopPropagation()
     
     })
 
+
+    const tSearch = document.querySelector('.search')
+    tSearch.addEventListener('input', function () {
+      buscarArticulo(this)
+      pintarCards(prodFiltrados)
+      e.stopPropagation()
+    } )
 /* HASTA ACA """""ORDERNADO"""""  */
 
 
   
+
+	const pintarMenu = productos => {
+
+	  categorias.push(listarCategorias(productos))
+	  categorias.pop()
+	  categorias.forEach( e => {
+      let x = document.createElement('li')
+      x.textContent = [e]
+      x.setAttribute('value',[e])
+      x.setAttribute('class', 'dropdown-item text-style item-menu')
+	    fragment.appendChild(x);
+	});
+  
+	navMenu.appendChild(fragment)
+	
+  } 
+
+const navMenu = document.querySelector('.menuProductos')
+navMenu.addEventListener('click',(e) => {
+  filtrarMenu(e.target.getAttribute('value'))
+})
+
+const filtrarMenu = ((categoria) => {
+  
+    let tempCategoria = categoria
+    pintarCards(prodFiltrados.filter(function(e) {
+        if(e.categoria === tempCategoria){
+            return (e)
+          }
+      }))
+      
+    })
+  
+
+// Usar para optimizar
+/* const filtrarProductos = ((arr, categoria) => {
+  arr.filter(function(e) {
+    if(e.categoria === tempCategoria){
+        return (e)
+      }
+  })
+}) */
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
 
 
 
